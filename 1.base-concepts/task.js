839f33d9d4c381b7d8dc0932bbd71f1da9376aab
AV <andrey.v730@gmail.com>
2023-06-29 22:48:40 +0300

    Fix spaces

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,20 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d;
+  let arr = [];
+  let d;
 
-	d = Math.pow(b, 2) - 4 * a * c;
+  d = Math.pow(b, 2) - 4 * a * c;
 
-	if (d === 0) {
-		arr[0] = -b / (2 * a);
-	}
-	else if (d > 0) {
-		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
-		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
-	}
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
+  }
+  else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
 
-	return arr;
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
