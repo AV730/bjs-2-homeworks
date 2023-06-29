6699be3279a15d27d3fcf3f5a06b54c73120ed6c
AV <andrey.v730@gmail.com>
2023-06-29 15:35:37 +0300

    First function done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let D;
+
+	D = Math.sqrt(b) - 4 * a * c;
+
+	if (D === 0) {
+		arr[0] = -b / (2 * a);
+	}
+	else if(D > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
