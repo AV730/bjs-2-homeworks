caa186011834fd59d098331ea7ed15d49f30f3d0
AV <andrey.v730@gmail.com>
2023-07-03 18:15:01 +0300

    2.Functions - task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,8 @@
 function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = arr.reduce((sum, current) => sum + current, 0);
+  let avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
